342dade2b7f787869e591a59834d15bde100192e
nikhil karkra <karkra.nikhil@gmail.com>
2023-06-02 20:09:36 +1000

    Update orgList.js
    Updating typos

--- src/orgList.js
+++ src/orgList.js
@@ -1,118 +1,104 @@
 const LIST = [
-    {
-        name:"Salesforce Demo Org",
-        subtext:"A full-featured copy of the Platform, for free",
-        link:"https://developer.salesforce.com/signup"
-    },
-    {
-        name:"Pardot Demo Org",
-        subtext:"Pardot provides a full suite of tools that help marketers create meaningful connections, generate more pipeline, and empower sales to close more deals.",
-        link:"https://trailhead.salesforce.com/promo/orgs/pardot-de"
-    },
-    {
-        name:"CPQ Demo Org",
-        subtext:"Salesforce CPQ simplifies complex sales processes by automating quoting, pricing, and configuration, resulting in faster sales cycles and increased revenue.",
-        link:"https://trailhead.salesforce.com/promo/orgs/cpqtrails"
-    },
-    {
-        name:"OmniStudio Demo Org",
-        subtext:"Salesforce Omnistudio provides a unified platform for creating and delivering exceptional customer experiences across multiple channels and touchpoints.",
-        link:"https://trailhead.salesforce.com/promo/orgs/omnistudiotrails"
-    },
-    {
-        name:"Feedback Management Trail Org",
-        subtext:"Better understand your customers' needs with Feedback Management in a full featured, unconfigured 15-day free trial",
-        link:"https://www.salesforce.com/form/signup/feedback-management-base-trial/"
-    },
-    {
-        name:"Net Zero Cloud Trail Org",
-        subtext:"Carbon is just the beginning. With Net Zero Cloud, you can integrate a complete sustainability management solution into your organization.",
-        link:"https://www.salesforce.com/form/signup/netzero-cloud-prerelease-trial/"
-    },
-    {
-        name:"Loyalty Management Trail Org",
-        subtext:"With Loyalty Management, your business can engage with customers in meaningful ways — bringing value to their experience and to your business.",
-        link:"https://www.salesforce.com/form/signup/loyalty-prerelease-trial/"
-    },
-    {
-        name:"Public Sector Solutions Trail Org",
-        subtext:"Public Sector Solutions are purpose-built applications built on the world’s most trusted CRM designed to help public sector organizations build and maintain thriving communities.",
-        link:"https://www.salesforce.com/form/signup/pubsec-prerelease-trial/"
-    },
-    {
-        name:"Salesforce Scheduler Trail Org",
-        subtext:"Salesforce Scheduler optimizes and automates appointment scheduling, ensuring efficient resource allocation and seamless customer experiences.",
-        link:"https://www.salesforce.com/form/signup/scheduler-prerelease-trial/"
-    },
-    {
-        name:"Health Cloud Trail Org",
-        subtext:"Health Cloud by Salesforce enables healthcare organizations to deliver personalized and connected care, streamline patient management, and improve overall health outcomes.",
-        link:"https://www.salesforce.com//form/signup/health-cloud-prerelease-trial/",
-      
-    },
-    {
-        name:"Education Cloud Trail Org",
-        subtext:"Education Cloud by Salesforce empowers educational institutions with a comprehensive platform for student management, engagement, and personalized learning experiences.",
-        link:"https://www.salesforce.com/form/sfdo/signup/education/pre-trial-ecda/",
-
-    },
-    {
-        name:"CRM Analytics Trail Org",
-        subtext:"Salesforce CRM Analytics leverages data-driven insights to empower businesses with actionable intelligence, enhance customer relationships, and drive informed decision-making.",
-        link:"https://www.salesforce.com/form/signup/freetrial-crm-analytics/",
-
-    },
-    {
-        name:"Mulesoft Trail Org",
-        subtext:"MuleSoft enables seamless integration and connectivity between various systems, applications, and data sources, enhancing agility and enabling digital transformation.",
-        link:"https://anypoint.mulesoft.com/login/signup",
-  
-    },
-    {
-        name:"Tableau Trail Org",
-        subtext:"Tableau is a powerful data visualization and analytics platform that helps businesses gain insights from their data through intuitive visualizations and interactive dashboards.",
-        link:"https://www.tableau.com/products/trial",
-    
-    },
-    {
-        name:"Financial Services Cloud Trail Org",
-        subtext:"Financial Services Cloud by Salesforce provides a comprehensive platform for financial institutions, empowering advisors with a 360-degree view of clients, personalized financial planning, and streamlined wealth management.",
-        link:"https://developer.salesforce.com/free-trials/comparison/financial-services-cloud",
-        
-    },
-    {
-        name:"Manufacturing Cloud Cloud Trail Org",
-        subtext:"Manufacturing Cloud by Salesforce delivers a tailored solution for manufacturers, enabling end-to-end visibility, efficient planning, and streamlined collaboration across the supply chain for improved operational efficiency.",
-        link:"https://developer.salesforce.com/free-trials/comparison/manufacturing-cloud",
-      
-    },
-    {
-        name:"Consumer Goods Cloud  Trail Org",
-        subtext:"Consumer Goods Cloud by Salesforce empowers consumer goods companies with comprehensive sales and analytics tools, enabling them to drive revenue growth, improve retail execution, and enhance collaboration with retail partners",
-        link:"https://developer.salesforce.com/free-trials/comparison/consumer-goods-cloud",
-     
-    },
-    {
-        name:"Energy & Utilities Cloud Trail Org",
-        subtext:"Energy & Utilities Cloud by Salesforce offers a unified platform for energy and utility companies, enabling them to optimize operations, improve customer engagement, and drive sustainable growth in the evolving energy landscape",
-        link:"https://www.salesforce.com/form/signup/energy-and-utilities-cloud-trial/",
-      
-
-    },
-    {
-        name:"Media Cloud Trail Org",
-        subtext:"Media Cloud by Salesforce provides media and entertainment companies with a centralized platform for content management, audience engagement, and data-driven insights to drive content strategy and enhance viewer experiences.",
-        link:"https://www.salesforce.com/form/industries/media/media-cloud-base-trial-v2/",
-      
-
-    },
-    {
-        name:"Communications Cloud Trail Org",
-        subtext:"Communications Cloud by Salesforce empowers telecommunications and communication service providers with a unified platform for customer engagement, personalized experiences, and efficient management of communication services.",
-        link:"https://www.salesforce.com/form/industries/communications/communications-cloud-base-trial-v2/",
-       
-
-    }
+  {
+    "name": "Salesforce Demo Org",
+    "subtext": "A full-featured copy of the Platform, for free",
+    "link": "https://developer.salesforce.com/signup"
+  },
+  {
+    "name": "Pardot Demo Org",
+    "subtext": "Pardot provides a full suite of tools that help marketers create meaningful connections, generate more pipeline, and empower sales to close more deals.",
+    "link": "https://trailhead.salesforce.com/promo/orgs/pardot-de"
+  },
+  {
+    "name": "CPQ Demo Org",
+    "subtext": "Salesforce CPQ simplifies complex sales processes by automating quoting, pricing, and configuration, resulting in faster sales cycles and increased revenue.",
+    "link": "https://trailhead.salesforce.com/promo/orgs/cpqtrails"
+  },
+  {
+    "name": "OmniStudio Demo Org",
+    "subtext": "Salesforce Omnistudio provides a unified platform for creating and delivering exceptional customer experiences across multiple channels and touchpoints.",
+    "link": "https://trailhead.salesforce.com/promo/orgs/omnistudiotrails"
+  },
+  {
+    "name": "Feedback Management Trial Org",
+    "subtext": "Better understand your customers' needs with Feedback Management in a full featured, unconfigured 15-day free trial",
+    "link": "https://www.salesforce.com/form/signup/feedback-management-base-trial/"
+  },
+  {
+    "name": "Net Zero Cloud Trial Org",
+    "subtext": "Carbon is just the beginning. With Net Zero Cloud, you can integrate a complete sustainability management solution into your organization.",
+    "link": "https://www.salesforce.com/form/signup/netzero-cloud-prerelease-trial/"
+  },
+  {
+    "name": "Loyalty Management Trial Org",
+    "subtext": "With Loyalty Management, your business can engage with customers in meaningful ways — bringing value to their experience and to your business.",
+    "link": "https://www.salesforce.com/form/signup/loyalty-prerelease-trial/"
+  },
+  {
+    "name": "Public Sector Solutions Trial Org",
+    "subtext": "Public Sector Solutions are purpose-built applications built on the world’s most trusted CRM designed to help public sector organizations build and maintain thriving communities.",
+    "link": "https://www.salesforce.com/form/signup/pubsec-prerelease-trial/"
+  },
+  {
+    "name": "Salesforce Scheduler Trial Org",
+    "subtext": "Salesforce Scheduler optimizes and automates appointment scheduling, ensuring efficient resource allocation and seamless customer experiences.",
+    "link": "https://www.salesforce.com/form/signup/scheduler-prerelease-trial/"
+  },
+  {
+    "name": "Health Cloud Trial Org",
+    "subtext": "Health Cloud by Salesforce enables healthcare organizations to deliver personalized and connected care, streamline patient management, and improve overall health outcomes.",
+    "link": "https://www.salesforce.com//form/signup/health-cloud-prerelease-trial/"
+  },
+  {
+    "name": "Education Cloud Trial Org",
+    "subtext": "Education Cloud by Salesforce empowers educational institutions with a comprehensive platform for student management, engagement, and personalized learning experiences.",
+    "link": "https://www.salesforce.com/form/sfdo/signup/education/pre-trial-ecda/"
+  },
+  {
+    "name": "CRM Analytics Trial Org",
+    "subtext": "Salesforce CRM Analytics leverages data-driven insights to empower businesses with actionable intelligence, enhance customer relationships, and drive informed decision-making.",
+    "link": "https://www.salesforce.com/form/signup/freetrial-crm-analytics/"
+  },
+  {
+    "name": "Mulesoft Trial Org",
+    "subtext": "MuleSoft enables seamless integration and connectivity between various systems, applications, and data sources, enhancing agility and enabling digital transformation.",
+    "link": "https://anypoint.mulesoft.com/login/signup"
+  },
+  {
+    "name": "Tableau Trial Org",
+    "subtext": "Tableau is a powerful data visualization and analytics platform that helps businesses gain insights from their data through intuitive visualizations and interactive dashboards.",
+    "link": "https://www.tableau.com/products/trial"
+  },
+  {
+    "name": "Financial Services Cloud Trial Org",
+    "subtext": "Financial Services Cloud by Salesforce provides a comprehensive platform for financial institutions, empowering advisors with a 360-degree view of clients, personalized financial planning, and streamlined wealth management.",
+    "link": "https://developer.salesforce.com/free-trials/comparison/financial-services-cloud"
+  },
+  {
+    "name": "Manufacturing Cloud Cloud Trial Org",
+    "subtext": "Manufacturing Cloud by Salesforce delivers a tailored solution for manufacturers, enabling end-to-end visibility, efficient planning, and streamlined collaboration across the supply chain for improved operational efficiency.",
+    "link": "https://developer.salesforce.com/free-trials/comparison/manufacturing-cloud"
+  },
+  {
+    "name": "Consumer Goods Cloud Trial Org",
+    "subtext": "Consumer Goods Cloud by Salesforce empowers consumer goods companies with comprehensive sales and analytics tools, enabling them to drive revenue growth, improve retail execution, and enhance collaboration with retail partners",
+    "link": "https://developer.salesforce.com/free-trials/comparison/consumer-goods-cloud"
+  },
+  {
+    "name": "Energy & Utilities Cloud Trial Org",
+    "subtext": "Energy & Utilities Cloud by Salesforce offers a unified platform for energy and utility companies, enabling them to optimize operations, improve customer engagement, and drive sustainable growth in the evolving energy landscape",
+    "link": "https://www.salesforce.com/form/signup/energy-and-utilities-cloud-trial/"
+  },
+  {
+    "name": "Media Cloud Trial Org",
+    "subtext": "Media Cloud by Salesforce provides media and entertainment companies with a centralized platform for content management, audience engagement, and data-driven insights to drive content strategy and enhance viewer experiences.",
+    "link": "https://www.salesforce.com/form/industries/media/media-cloud-base-trial-v2/"
+  },
+  {
+    "name": "Communications Cloud Trial Org",
+    "subtext": "Communications Cloud by Salesforce empowers telecommunications and communication service providers with a unified platform for customer engagement, personalized experiences, and efficient management of communication services.",
+    "link": "https://www.salesforce.com/form/industries/communications/communications-cloud-base-trial-v2/"
+  }
 ]
 
 export default LIST
